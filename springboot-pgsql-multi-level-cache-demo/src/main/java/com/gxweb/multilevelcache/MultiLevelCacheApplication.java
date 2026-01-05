7d76bec76c4aa0d4c75a24a714e591675132e748
cyq <cyq9516@gmail.com>
2026-01-05 16:18:56 +0800

    Add springboot-pgsql-multi-level-cache-demo 参考

--- springboot-pgsql-multi-level-cache-demo/src/main/java/com/gxweb/multilevelcache/MultiLevelCacheApplication.java
+++ springboot-pgsql-multi-level-cache-demo/src/main/java/com/gxweb/multilevelcache/MultiLevelCacheApplication.java
@@ -3,6 +3,9 @@ package com.gxweb.multilevelcache;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * https://mp.weixin.qq.com/s/43AaaFlE5-nVrBPX81sXsw
+ */
 @SpringBootApplication
 public class MultiLevelCacheApplication {
 
